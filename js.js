4dddfe189c3b515f8b634963c9eddb52a2243483
Juan Diego <jdalbinoramos@gmail.com>
2022-01-01 23:01:08 -0500

    project completed

---
+++ js.js
@@ -0,0 +1,87 @@
+const container = document.querySelector('.container')
+const btnBlack = document.createElement('button')
+const btnGray = document.createElement('button')
+const btnRGB = document.createElement('button')
+const btnSize = document.createElement('button')
+const buttonsContainer = document.querySelector('.buttons')
+
+function CreateDivs(col,row) {
+    for (let i = 0; i < (col * row); i++) {
+        const div = document.createElement('div')
+        div.style.border = '1px solid red'
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+        container.appendChild(div).classList.add('box')
+    }
+}
+
+CreateDivs(16,16)
+
+function grayColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnGray.textContent = "Gray"
+    btnGray.addEventListener('click' ,() => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let Rnum = Math.floor(Math.random() * 255)
+            box.style.background = `rgb(${Rnum},${Rnum},${Rnum})`
+        }))
+    })
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
+}
+grayColor()
+
+function blackColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.textContent = "Black"
+    btnBlack.addEventListener('click',() => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let Rnum = Math.floor(Math.random() * 255)
+            box.style.background = `black`
+        }))
+    })
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')   
+}
+blackColor()
+
+function rgbColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnRGB.textContent = "RGB"
+    btnRGB.addEventListener('click',() => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let R = Math.floor(Math.random() * 255)
+            let G = Math.floor(Math.random() * 255)
+            let B = Math.floor(Math.random() * 255)
+            box.style.background = `rgb(${R},${G},${B})`
+        }))
+    })
+    buttonsContainer.appendChild(btnRGB).classList.add('btn')   
+}
+rgbColor()
+
+function reSet() {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => box.remove())
+}
+
+function reSize() {
+    btnSize.textContent = 'GRID SIZE'
+    btnSize.addEventListener('click', () => {
+        let user = prompt('Do you Want to Reset Values?')
+        if (user === null || user < 1 || user > 100){
+            reSet()
+            CreateDivs(16,16)
+            blackColor()
+            grayColor()
+            rgbColor()
+            alert('Please submit a correct value')
+        } else {
+            reSet()
+            CreateDivs(user,user)
+            blackColor()
+            grayColor()
+            rgbColor()
+        }
+    })
+    buttonsContainer.appendChild(btnSize).classList.add('btn')
+}
+reSize()
